f34d6bd91469a5028fdcd6540be9f30c839d3f30
chantya127 <58797479+chantya127@users.noreply.github.com>
2022-01-31 17:39:17 +0530

    Time: 36 ms (68.83%), Space: 14 MB (95.10%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.py
@@ -0,0 +1,32 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        def swap(arr , p1,p2):
+            
+            arr[p1] ,arr[p2] = arr[p2] , arr[p1]
+            
+        
+        size = len(nums)
+        
+        st ,end = 0 , size-1
+        mid = 0
+        
+        while(mid <= end):
+            
+            curr = nums[mid]
+            
+            if(curr == 0):
+                
+                swap(nums , st ,mid)
+                st +=1
+                mid +=1
+            
+            elif(curr == 2):
+                swap(nums , end , mid)
+                end -=1
+            
+            else:
+                mid +=1
