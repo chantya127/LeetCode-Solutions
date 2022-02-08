b11d6b96c5b99eb67bb32333f21ce120583de61e
chantya127 <58797479+chantya127@users.noreply.github.com>
2022-02-08 11:43:51 +0530

    Time: 107 ms (52.10%), Space: 14 MB (85.64%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.py
@@ -0,0 +1,28 @@
+class Solution:
+    def combinationSum(self, cand: List[int], target: int) -> List[List[int]]:
+        
+        def solve(idx , summe , curr):
+            
+            if idx == size :
+                if (summe == target):
+                    ans.append(curr[:])
+                    
+                
+                return
+            
+            if (summe + cand[idx] <= target):
+                
+                solve(idx , summe+cand[idx] , curr + [cand[idx]])
+            
+            solve(idx+1 ,  summe , curr)
+            
+                
+                
+        
+        size = len(cand)
+        
+        ans = []
+        
+        solve(0 , 0,[])
+        
+        return ans
