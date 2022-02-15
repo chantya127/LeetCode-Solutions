d9e14bc4f944648f4bb6216ff89d088df9fabb9e
chantya127 <58797479+chantya127@users.noreply.github.com>
2022-02-15 13:37:23 +0530

    Time: 1597 ms (18.98%), Space: 79 MB (47.95%) - LeetHub

---
+++ 460-lfu-cache/460-lfu-cache.py
@@ -0,0 +1,184 @@
+
+class Node:
+    def __init__(self,key=-1, value=-1 , level =-1):
+        
+        self.next = None
+        self.prev = None
+        self.key = key
+        self.value = value
+        self.level = level
+        
+
+
+class LFUCache:
+
+    def __init__(self, capacity: int):
+        
+        self.level_cache = {}
+        self.key_cache = {}
+        
+        self.capacity = capacity
+        self.size = 0
+        
+
+    def get(self, key: int) -> int:
+        
+        if (key in self.key_cache):
+            
+            node = self.key_cache[key]
+            
+            node.level +=1
+            
+            curr_level = node.level
+
+            self.remove(node)
+            
+            prev_level = node.level-1
+            
+            if (prev_level in self.level_cache):
+                prev_head , prev_tail = self.level_cache[prev_level]
+            
+                # no values left in prev level
+                if(prev_head.next == prev_tail):
+                    self.level_cache.pop(prev_level)
+            
+            
+            # new entry in level_cache
+            if (curr_level not in self.level_cache):
+                
+                head = Node()
+                tail = Node()
+                
+                head.next = tail
+                tail.prev = head
+                
+                self.level_cache[curr_level] = [head,tail]
+            
+            else:
+                head,tail = self.level_cache[curr_level]
+            
+            self.key_cache[key] = node
+            self.insert(node, tail)
+            
+            # print(self.key_cache)
+            
+            return node.value
+        
+        else:
+            # print(self.key_cache , self.size)
+            return -1
+                
+
+    def put(self, key: int, value: int) -> None:
+        
+        
+        if (self.capacity == 0):
+            return
+        
+        if (key not in self.key_cache):
+            curr_level = 1
+            self.size +=1
+            node = Node(key,value,curr_level)
+        
+        else:
+            
+            node = self.key_cache[key]
+            node.value = value            
+            
+            node.level +=1
+            
+            curr_level = node.level
+
+            self.remove(node)
+            
+            prev_level = node.level-1
+            
+            if (prev_level in self.level_cache):
+                prev_head , prev_tail = self.level_cache[prev_level]
+            
+                # no values left in prev level
+                if(prev_head.next == prev_tail or prev_head.next == None):
+                    self.level_cache.pop(prev_level)
+            
+
+        if(self.size > self.capacity) and (key not in self.key_cache):
+            
+            # find minimum level in level_cache
+            
+            min_level = float('inf')
+            
+            for level in self.level_cache:
+                
+                min_level = min(min_level , level)
+                #print(level , min_level , 'zzzz')
+            
+            if min_level != float('inf'):
+                
+                self.size -=1
+                head,tail = self.level_cache[min_level]
+
+                if (head.next != tail and head.next != None):
+                    
+                    
+                    # remove least recently used node
+                    
+                    first_node = head.next
+                    #print(min_level ,first_node.key ,first_node.level, 'fff')
+                    self.remove(first_node)
+
+                    first_key = first_node.key
+                    self.key_cache.pop(first_key)
+
+
+                if (head.next == tail or head.next == None):
+                    self.level_cache.pop(min_level)
+        
+        
+        
+        if (curr_level not in self.level_cache):
+            
+            head = Node()
+            tail = Node()
+
+            head.next = tail
+            tail.prev = head
+            self.level_cache[curr_level] = [head,tail]
+            
+        else:
+            head,tail = self.level_cache[curr_level]
+            
+        self.key_cache[key] = node
+        self.insert(node, tail)
+        
+        # print(self.key_cache , key , 'ssss')
+        # print()
+    
+    def insert(self,new_node , tail):
+        
+        prev_node = tail.prev
+        
+        prev_node.next= new_node
+        new_node.prev = prev_node
+        
+        new_node.next = tail
+        tail.prev = new_node
+    
+    def remove(self,new_node):
+        
+        prev_node = new_node.prev
+        next_node = new_node.next
+        
+        #print(new_node.key , new_node.value )
+        
+        prev_node.next = next_node
+        next_node.prev = prev_node
+
+
+# Your LFUCache object will be instantiated and called as such:
+# obj = LFUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+
+# key = counter
+
+# value => Linked list of same counter values
