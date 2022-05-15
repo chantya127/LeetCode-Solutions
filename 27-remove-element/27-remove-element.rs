912120dfd81f73930fdf9345dcd566abcdb091ca
chantya127 <58797479+chantya127@users.noreply.github.com>
2022-05-15 18:43:13 +0530

    Time: 3 ms (8.37%), Space: 2.2 MB (9.77%) - LeetHub

--- 27-remove-element/27-remove-element.rs
+++ 27-remove-element/27-remove-element.rs
@@ -1,28 +1,7 @@
 impl Solution {
     pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
-        
-        let mut prev = 0;
-        let mut flag:bool = true;
-        // let mut val = &val;
-        
-        for index in 0..nums.len(){
-            if (nums[index] != val){
-                if (flag){
-                    flag = false;
-                }
-                else{
-                    prev +=1;
-                }
-                nums[prev] = nums[index];
-                
-            }
-        }
-        // println!("{:?}" , nums);
-        if (flag){
-            return 0;
-        }
-        else{
-            (prev+1) as i32
-        }
+     
+        nums.retain(|&x| x != val);
+        nums.len() as i32
     }
 }
