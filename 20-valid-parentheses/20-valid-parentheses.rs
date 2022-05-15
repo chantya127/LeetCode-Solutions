86af8773417d86c62e525bafb4cf49156cf4ef1b
chantya127 <58797479+chantya127@users.noreply.github.com>
2022-05-15 18:10:45 +0530

    Time: 0 ms (100.00%), Space: 2.3 MB (16.07%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        
+        let mut stack = Vec::new();
+        
+        for char in s.chars(){
+            match char {
+                '{' => stack.push('}'),
+                '[' => stack.push(']'),
+                '(' => stack.push(')'),
+                '}'|']'|')' if Some(char) != stack.pop() =>return false,
+                _ => (),
+                
+            }
+        }
+        return stack.is_empty();
+    }
+}
