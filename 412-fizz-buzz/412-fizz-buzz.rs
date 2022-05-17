54b924ed887fccea2597afdfaaf1cf8cdcfb33c5
chantya127 <58797479+chantya127@users.noreply.github.com>
2022-05-17 19:28:37 +0530

    Time: 4 ms (27.45%), Space: 2.8 MB (32.35%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.rs
+++ 412-fizz-buzz/412-fizz-buzz.rs
@@ -1,27 +1,15 @@
 impl Solution {
     pub fn fizz_buzz(n: i32) -> Vec<String> {
-        let mut ans:Vec<String> = Vec::new();
-    
-    for idx in 1..n+1{
-
-        let mut curr_ans = String::new();
-
-        if idx%3 ==0 && idx%5 ==0{
-            curr_ans += "FizzBuzz";
+        return (1..=n).map(|x| {
+        let by3 = x%3==0;
+        let by5 = x%5==0;
+        
+        match (by3 , by5){
+            (true,true) => "FizzBuzz".to_string(),
+            (true,false) =>"Fizz".to_string(),
+            (false,true) => "Buzz".to_string(),
+            _ => x.to_string(),
         }
-        else if idx%3 == 0 {
-            curr_ans += "Fizz";
-        }
-        else if idx %5 == 0{
-            curr_ans += "Buzz";
-        }
-        else{
-            curr_ans += &idx.to_string();
-        }
-
-        ans.push(curr_ans)
-
-    }
-    ans
+    }).collect();
     }
 }
