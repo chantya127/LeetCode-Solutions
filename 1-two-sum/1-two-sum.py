219aa61c15df69acdc7f62acdb2184dab6580a0d
chantya127 <58797479+chantya127@users.noreply.github.com>
2022-07-30 12:17:42 +0530

    Time: 115 ms (49.03%), Space: 15.3 MB (24.31%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,21 +1,14 @@
+from collections import defaultdict
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
-        size = len(nums)
+        vis = defaultdict(int)
         
-        indices = {}
+        for (idx , num) in enumerate(nums):
+            if (target - num) in vis:
+                # print(target , num , idx , vis)
+                return [vis[target-num] , idx]
+            
+            vis[num] = idx
         
-        for idx in range(size):
-            
-            curr = nums[idx]
-            
-            rem = target - curr
-            
-            if (rem in indices):
-                return [indices[rem] , idx]
-            
-            if curr not in indices:
-                indices[curr] = idx
-        
-                    
