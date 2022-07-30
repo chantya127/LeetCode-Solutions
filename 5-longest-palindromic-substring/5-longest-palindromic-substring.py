e889e8bf9b474166c91b24c1a4fda11e31f3129c
chantya127 <58797479+chantya127@users.noreply.github.com>
2022-07-30 12:33:38 +0530

    Time: 791 ms (90.16%), Space: 14 MB (29.35%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,34 +1,42 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         
-        start ,maxlen = 0,1
+        start_idx , end_idx = 0,0
+        max_len = 1
         
         size = len(s)
         
-        for idx in range(1,size):
+        for idx in range(size):
             
-            #even length
-            low = idx-1
-            high = idx
+            # even start
             
-            while(low >=0 and high < size and s[low] == s[high]):
-                if(high-low +1 > maxlen):
-                    
-                    maxlen = high-low+1
-                    start = low
-                high +=1
-                low -=1
+            ptr1 = idx
+            ptr2 = idx +1
             
-            # odd length
-            low = idx-1
-            high = idx+1
+            while(ptr1 >=0 and ptr2 < size and s[ptr1] == s[ptr2]):
+                ptr1 -=1
+                ptr2 += 1
             
-            while(low >=0 and high < size and s[low] == s[high]):
-                if(high-low +1 > maxlen):
-                    
-                    maxlen = high-low+1
-                    start = low
-                high +=1
-                low -=1
-        
-        return s[start:start + maxlen]
+            curr_len = ptr2 -ptr1 -1
+            if (curr_len > max_len):
+                start_idx  = ptr1 +1 
+                max_len = curr_len
+            
+            
+            # odd_start
+            ptr1 = idx-1
+            ptr2 = idx+1
+            
+            while(ptr1 >=0 and ptr2 < size and s[ptr1] == s[ptr2]):
+                ptr1 -=1
+                ptr2 +=1
+            
+            curr_len = ptr2-ptr1-1
+            if (curr_len > max_len):
+                start_idx  = ptr1+1
+                max_len = curr_len
+            
+            
+        print(start_idx , end_idx , max_len)
+        return s[start_idx:start_idx+max_len]
+            
