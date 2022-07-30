e163937e69ec170ed4555877700df849e54e7216
chantya127 <58797479+chantya127@users.noreply.github.com>
2022-07-30 13:39:11 +0530

    Time: 163 ms (58.41%), Space: 17.7 MB (61.07%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -1,34 +1,39 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
 from heapq import heappush as push , heappop as pop
 
+
 class Solution:
-    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         
-        def solve(lists , pos , n):
-            
-            n = len(lists) 
-            mhead = mtail = ListNode(-1)
-            heap = []
-            
-            for i in range(n):
-                if lists[i]:
-                    push(heap , (lists[i].val ,i , lists[i]))
-            
-            while(heap):
-                
-                val , _, node = pop(heap)
-                mtail.next = node
-                mtail = mtail.next
-                if (node.next):
-                    i+=1
-                    push(heap , (node.next.val, i , node.next))
-                
-                mtail.next = None
-            
-            return mhead.next
-                
-            
         
-        if len(lists) == 0:
+        heap = []
+        size = len(lists)
+        if (size == 0):
             return None
         
-        return solve(lists, 0 , len(lists))
+        for idx in range(size):
+            
+            if  (lists[idx]):
+                push(heap, (lists[idx].val,idx ,lists[idx]))
+        
+        mhead = ListNode(-1)
+        mtail = mhead
+                
+        while(heap):
+            
+            val ,_,node = pop(heap)
+            mtail.next = node
+            mtail = mtail.next
+            
+            if (node.next):
+                idx +=1
+                push(heap, (node.next.val,idx , node.next))
+            
+            mtail.next = None
+            
+        return mhead.next
